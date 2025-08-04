96067f6c2db68d84d3b6e106c68ef3e00d50b02c
While-tru <bhumiawasthi785@gmail.com>
2025-08-04 01:07:46 -0400

    Added a feature to read and extract emotion data from Watson NLP API response

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -14,5 +15,32 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, headers=headers, json=json_data)
 
-    # Return the text content of the response
-    return response.text
+    #Conver to py dict
+    response_dict = json.loads(response.text)
+    #Extract emotion score
+    emotion_scores = response_dict['emotionPredictions'][0]['emotion']
+    #Extract req emotions
+    anger = emotion_scores.get('anger', 0)
+    disgust = emotion_scores.get('disgust', 0)
+    fear = emotion_scores.get('fear', 0)
+    joy = emotion_scores.get('joy', 0)
+    sadness = emotion_scores.get('sadness', 0)
+
+    #Determining dominant emotion
+    emotions = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
